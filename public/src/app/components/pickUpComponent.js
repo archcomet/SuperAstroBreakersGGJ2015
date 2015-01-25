f7533ebdb5cc9102f88d6fb39acd95b8d0dee841
michaelg <archcomet@gmail.com>
2015-01-25 14:25:47 -0800

    Merge remote-tracking branch 'remotes/origin/master' into archcomet
    Conflicts:
    	public/src/gameConfig.js

--- public/src/app/components/pickUpComponent.js
+++ public/src/app/components/pickUpComponent.js
@@ -14,13 +14,24 @@ define([
             this.color      = options.color || { value: 0x0000ff, name: 'blue' };
             this.emissive   = options.emissive || 0x110033;
 
-            var geometry = new THREE.SphereGeometry(options.radius, 5, 5);
-            var material = new THREE.MeshPhongMaterial({
-                color: this.color.value,
-                emissive: this.emissive
+            var geometry = new THREE.TorusGeometry(options.radius, 7, 8, 16 );
+            var material = new THREE.MeshBasicMaterial({
+                color: this.color.value
             });
 
-            this.mesh = new THREE.Mesh(geometry, material);
+            this.ring1 = new THREE.Mesh(geometry, material);
+
+            this.ring2 = new THREE.Mesh(geometry, material);
+            this.ring2.scale.multiplyScalar(0.6);
+
+            this.ring3 = new THREE.Mesh(geometry, material);
+            this.ring3.scale.multiplyScalar(0.6);
+
+            this.mesh = new THREE.Object3D();
+            this.mesh.add(this.ring1);
+            this.ring1.add(this.ring2);
+            this.ring2.add(this.ring3);
+
             this.duration = options.duration;
         }
 
